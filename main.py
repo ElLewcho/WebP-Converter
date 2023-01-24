811b5fcc3035ac62613bbdbe4dee2c0bba40e7a8
ElLewcho <46673467+ElLewcho@users.noreply.github.com>
2023-01-24 16:48:00 +0000

    Adjusted GUI dimensions

--- main.py
+++ main.py
@@ -4,15 +4,18 @@ from tkinter import messagebox
 from PIL import Image
 from tkinter import ttk
 from concurrent.futures import ThreadPoolExecutor
+import threading
 
 root = tk.Tk()
-root.title("Image Converter")
+root.title("Lew's WebP Converter")
+root.geometry("400x200")
 
 quality_label = tk.Label(root, text="Quality (0-100)")
 quality_label.pack()
 
 quality_input = tk.Entry(root)
 quality_input.pack()
+
 
 def select_files():
     files = filedialog.askopenfilenames(title = "Select Images", filetypes = (("JPEG files", "*.jpg"), ("PNG files", "*.png"), ("All files", "*.*")))
@@ -40,7 +43,6 @@ def convert_images(files, quality, output_folder, progress_bar):
     if not quality or not quality.isnumeric() or int(quality) < 0 or int(quality) > 100:
         messagebox.showerror("Error", "Please enter a valid quality value between 0 and 100")
         return
-        
     total = len(files)
     progress = 0
     with ThreadPoolExecutor() as executor:
@@ -67,7 +69,8 @@ def start_conversion():
     progress_bar = ttk.Progressbar(root, orient="horizontal", length=200, mode="determinate")
     progress_bar.pack()
     progress_bar["maximum"] = len(files)
-    convert_images(files, quality, folder, progress_bar)
+    thread = threading.Thread(target=convert_images, args=(files, quality, folder, progress_bar))
+    thread.start()
 
 select_files_button = tk.Button(root, text="Select Files", command=start_conversion)
 select_files_button.pack()
